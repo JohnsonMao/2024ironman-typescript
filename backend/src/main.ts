c9fa59401e5ccce0aaae6a033938d618f8cec8a4
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2024-09-26 22:59:05 +0800

    refactor: typescript 5 decorator (#5)
    * chore: modify config and add polyfill
    
    * feature: add ts5 controller decorator
    
    * refactor: router and controllers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,31 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import { ZodError } from 'zod';
-import taskRouter from './routes/task.route';
+import { controllers } from './controllers';
+import { validateMetadata } from './utils/decorators/controller.decorator';
 
 const port = process.env.PORT || 9453;
 
 class Server {
     private app = express();
 
-    start() {
-        this.app.use(express.json());
+    private registerRoutes() {
+        controllers.forEach((Controller) => {
+            const controller = new Controller();
+            const { basePath, routers } = validateMetadata(Controller);
+            const router = Router();
 
-        this.app.use('/task', taskRouter);
+            routers.forEach(({ method, path, handlerName }) => {
+                router[method](
+                    path,
+                    controller[handlerName as keyof typeof controller].bind(controller)
+                );
+            });
 
+            this.app.use(basePath, router);
+        });
+    }
+
+    private errorMiddleware() {
         this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
             if (err instanceof ZodError) {
                 res.status(400).json({ message: err.errors });
@@ -19,6 +33,14 @@ class Server {
                 res.status(500).json({ message: err.message });
             }
         });
+    }
+
+    start() {
+        this.app.use(express.json());
+
+        this.registerRoutes();
+
+        this.errorMiddleware();
 
         this.app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
