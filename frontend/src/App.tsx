ee49f26a3454f7fb8241b6e904c214d118d13d9b
JohnsonMao <johnsonmao@nine-yi.com>
2024-09-29 21:32:23 +0800

    feature: add task page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,8 @@
-import { useEffect, useState } from 'react';
-import TaskList from './features/task/TaskList';
-import { type TaskType, getTasks } from './services/task.service';
+import Task from './pages/task.page';
 import './App.css';
 
 function App() {
-    const [tasks, setTasks] = useState<TaskType[]>([]);
-
-    useEffect(() => {
-        getTasks().then(setTasks);
-    }, []);
-
-    return (
-        <div className="container">
-            <h1>Task Board</h1>
-            <TaskList tasks={tasks} />
-        </div>
-    );
+    return <Task />;
 }
 
 export default App;
