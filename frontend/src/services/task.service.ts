6672cd514d9cc3ffe539dc938e89bc028e3d4780
JohnsonMao <johnsonmao@nine-yi.com>
2024-09-29 21:31:57 +0800

    feature: add http utils

--- frontend/src/services/task.service.ts
+++ frontend/src/services/task.service.ts
@@ -1,24 +1,28 @@
-import type { TaskType } from '@models/task.model';
+import type { TaskType as _TaskType } from '@models/task.model';
+import http from '@/utils/createHttp';
+
+export type TaskType = _TaskType;
+
+const taskPath = '/task';
+
+export const createTask = async (task: Omit<TaskType, 'id'>): Promise<string> => {
+    const { id } = await http.post<{ id: string }>(taskPath, task);
+    return id;
+};
 
 export const getTasks = async (): Promise<TaskType[]> => {
-    return [
-        {
-            id: '1',
-            title: '鐵人賽 Day 27',
-            description: 'TypeScript 實戰：前端表單與串接 API',
-            status: 'new',
-        },
-        {
-            id: '2',
-            title: '鐵人賽 Day 26',
-            description: 'TypeScript 實戰：前端泛型元件',
-            status: 'active',
-        },
-        {
-            id: '3',
-            title: '鐵人賽 Day 25',
-            description: 'TypeScript 實戰：前端 React Setup',
-            status: 'completed',
-        },
-    ];
+    return http.get<TaskType[]>(taskPath);
+};
+
+export const getTaskById = async (id: string): Promise<TaskType> => {
+    return http.get<TaskType>(`${taskPath}/${id}`);
+};
+
+export const updateTask = async (task: TaskType): Promise<void> => {
+    const { id, ...rest } = task;
+    return http.put(`${taskPath}/${id}`, rest);
+};
+
+export const deleteTaskById = async (id: string): Promise<void> => {
+    return http.delete(`${taskPath}/${id}`);
 };
