f914fb521d583cf19eeef0e1eb31e29aa19e6e53
JohnsonMao <johnsonmao@nine-yi.com>
2024-09-30 23:44:21 +0800

    feature: use package

--- backend/src/models/task.model.ts
+++ backend/src/models/task.model.ts
@@ -1,12 +1,6 @@
 import { z } from 'zod';
+import { inputTaskSchema } from 'validator';
 import AbstractBaseModel from '../utils/abstract/base.model';
-
-export const inputTaskSchema = z.object({
-    title: z.string().min(1, '請輸入任務標題'),
-    description: z.string().optional(),
-    status: z.enum(['new', 'active', 'completed']).default('new'),
-    storyPoint: z.number().optional(),
-});
 
 export interface TaskType extends z.infer<typeof inputTaskSchema> {
     id: string;
